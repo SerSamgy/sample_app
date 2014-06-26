acb6a081de037475d82a48f548b33783ffa3e62d
Samgy <samgy-cool@yandex.ru>
2014-06-26 17:07:51 +0400

    Add profile page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+  	name 					"Semyon Gaivoronskiy"
+  	email 					"samgy7cool@gmail.com"
+  	password 				"foobar"
+  	password_confirmation 	"foobar"
+  end
+end
